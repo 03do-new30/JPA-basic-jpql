cc20f53666aacedb6b17d2a9aeafb7e5d395e7d9
donew <eastgloss0330@naver.com>
2024-04-17 19:23:59 +0900

    [Feat] case, coalesce, nullif

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -21,27 +21,46 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername("member1");
             member.setAge(10);
             member.setTeam(team);
             member.setMemberType(MemberType.ADMIN);
 
+            Member member2 = new Member();
+            member2.setUsername("관리자");
+            member2.setAge(20);
+
             em.persist(member);
+            em.persist(member2);
 
             // 영속성 컨텍스트 비운다
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'Hello', true from Member m " +
-                    "where m.memberType = :userType";
-            List<Object[]> resultList = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
-                    .getResultList();
-            for (Object[] objects : resultList) {
-                System.out.println("objects[0] = " + objects[0]);
-                System.out.println("objects[1] = " + objects[1]);
-                System.out.println("objects[2] = " + objects[2]);
+            // 기본 CASE식
+            String query = "select " +
+                    "case when m.age <= 10 then '학생요금' " +
+                    "when m.age >= 60 then '경로요금' " +
+                    "else 'common' " +
+                    "end " +
+                    "from Member m";
+            List<String> resultList = em.createQuery(query).getResultList();
+            for (String s : resultList) {
+                System.out.println("s = " + s);
             }
+            // COALESCE - 사용자 이름이 없으면 이름 없는 회원을 반환한다
+            String query2 = "select coalesce(m.username, '이름 없는 회원') from Member m";
+            List<String> resultList2 = em.createQuery(query2).getResultList();
+            for (String s : resultList2) {
+                System.out.println("coalesce 결과 = " + s);
+            }
+            // NULLIF - 두 값이 같으면 null을 반환하고 아니면 본인의 데이터를 반환
+            String query3 = "select NULLIF(m.username, '관리자') from Member m";
+            List<String> resultList3 = em.createQuery(query3).getResultList();
+            for (String s : resultList3) {
+                System.out.println("nullif 결과 = " + s);
+            }
+
 
             tx.commit(); // 커밋 시점에 INSERT (버퍼링 가능)
         } catch (Exception e) {
