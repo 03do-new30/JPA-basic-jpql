5f90c54b5fda6db578b999e43eef20766ebb6c8b
donew <eastgloss0330@naver.com>
2024-04-17 15:55:53 +0900

    [Feat] Member, Team 엔티티

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,33 @@
+package jpql;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            tx.commit(); // 커밋 시점에 INSERT (버퍼링 가능)
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close(); // 영속성 컨텍스트를 종료
+        }
+        emf.close();
+    }
+}
---
+++ src/main/java/jpql/Team.java
@@ -0,0 +1,36 @@
+package jpql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members  = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
