993b75181a264d990c5f18637fb5d856aa9c69f2
donew <eastgloss0330@naver.com>
2024-04-17 16:52:06 +0900

    [Feat] 프로젝션 - 영속성 컨텍스트 관점에서

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -21,13 +21,19 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
+            // 영속성 컨텍스트 비운다
+            em.flush();
+            em.clear();
 
-            // 파라미터 바인딩 - 이름 기준
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("singleResult = " + result.getUsername());
-
+            // 아래처럼 데이터를 가져오면 이 가져온 Member는 영속성 컨텍스트에 관리가 될까?
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+            Member findMember = result.get(0);
+            // DB에 변경사항이 반영되면 영속성 컨텍스트에 관리되는거고
+            // 반영되지 않으면 관리되지 않는 것이다
+            findMember.setAge(20);
+            // update쿼리가 나간다! = 관리된다
+            // 프로젝션하는 값들이 다 영속성 컨텍스트에서 관리된다고 볼 수 있다
 
             tx.commit(); // 커밋 시점에 INSERT (버퍼링 가능)
         } catch (Exception e) {
