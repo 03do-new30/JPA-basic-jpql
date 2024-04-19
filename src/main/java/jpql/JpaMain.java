7678cadd194c1c022e6082771c4ff9101c16c9e7
donew <eastgloss0330@naver.com>
2024-04-19 20:14:53 +0900

    [Feat] 네임드 쿼리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -52,39 +52,13 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // 엔티티 직접 사용 - 기본 키 값으로 사용된다
-            // (1) 엔티티를 파라미터로 전달
-            String query = "select m from Member m where m = :member";
-            Member findMember = em.createQuery(query, Member.class)
-                    .setParameter("member", member1)
-                    .getSingleResult();
-            System.out.println("findMember = " + findMember);
-            // (2) 식별자로 직접 전달
-            String query2 = "select m from Member m where m.id = :member";
-            Member findMember2 = em.createQuery(query2, Member.class)
-                    .setParameter("member", member1.getId())
-                    .getSingleResult();
-            System.out.println("findMember = " + findMember2);
-            // 둘 다 똑같은 쿼리
-
-            // 엔티티 직접 사용 - 외래 키 값
-            // TEAM_ID = 팀아이디
-            // (1) 엔티티 전달
-            String query3 = "select m from Member m where m.team = :team";
-            List<Member> resultList = em.createQuery(query3, Member.class)
-                    .setParameter("team", teamA)
-                    .getResultList();
+            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
+                    .setParameter("username", "회원1").
+                    getResultList();
             for (Member member : resultList) {
-                System.out.println("member = " + member);
+                System.out.println(member);
             }
-            // (2) 식별자 전달
-            String query4 = "select m from Member m where m.team.id = :teamId";
-            List<Member> resultList1 = em.createQuery(query4, Member.class)
-                    .setParameter("teamId", teamA.getId())
-                    .getResultList();
-            for (Member member : resultList1) {
-                System.out.println("member = " + member);
-            }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
